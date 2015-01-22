6cfe7a72418f850f201d9fe16c78a124f5d4496e
przemekch92 <przemyslaw.p.chudzik@gmail.com>
2015-01-22 11:05:08 +0100

    Dodalem klase Pojazd

---
+++ WypozyczalniaSamochodow/src/wypozyczalniasamochodow/Pojazd.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wypozyczalniasamochodow;
+
+/**
+ *
+ * @author Przemysław Chudzik
+ */
+public class Pojazd {
+    private String marka;
+    private String model;
+    private int rokProdukcji;
+    private int nrSeryjny;
+    private String nrRejestracyjny;
+    
+    
+    public Pojazd(String marka, String model, int rokProdukcji, int nrSeryjny, String nrRejestracyjny){
+        marka = this.marka;
+        model = this.model;
+        rokProdukcji = this.rokProdukcji;
+        nrSeryjny = this.nrSeryjny;
+        nrRejestracyjny = this.nrRejestracyjny;
+    }//public Pojazd
+
+    public Pojazd() {
+        this.marka = "aaa";
+        this.model = "bbb";
+        this.nrRejestracyjny = "ABC123";
+        this.rokProdukcji = 2000;
+        this.nrSeryjny = 123456789;    
+    }
+}//public class Pojazd
