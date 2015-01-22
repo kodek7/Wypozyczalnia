15fdb42a41c8bf14e387802d8d83ab4f5468ae26
Hollow1991 <cielakjakub@gmail.com>
2015-01-22 11:14:05 +0100

    klasa klienci

---
+++ WypozyczalniaSamochodow/src/wypozyczalniasamochodow/Klient.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wypozyczalniasamochodow;
+
+/**
+ *
+ * @author Zdybel Grzegorz, Przemysław Chudzik
+ */
+
+public class Klient {
+    //pola
+    private String Imie;
+    private String Nazwisko;
+    private String Miejsowosc;
+    private String Adres;
+    private int Pesel;
+    
+    //konstruktor
+    public Klient(String Imie, String Nazwisko, String Miejscowosc, String Adres, int Pesel)
+    {
+        this.Imie=Imie;
+        this.Nazwisko=Nazwisko;
+        this.Miejsowosc=Miejscowosc;
+        this.Adres=Adres;
+        this.Pesel=Pesel;
+    }//public Klienci
+    //konstruktor bezparametrowy
+    public Klient()
+    {
+     this.Adres="nieznany";
+     this.Imie="brak";
+     this.Nazwisko="brak";
+     this.Miejsowosc="brak";
+     this.Pesel=0;
+    }//public klienci
+}//public class Klienci
